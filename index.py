6a4203c7ecc8789879fe5238928ff7bbdc177f51
Rob Elliott <elliott@iupui.edu>
2023-08-25 12:50:53 -0400

    Start code

--- index.py
+++ index.py
@@ -1,17 +1,6 @@
-import shapes
 
 def main():
-    r1_area = shapes.area_rectangle(4,5)
-    print(f"The area of a rectangle with sides 4 and 5 is {r1_area}")
-
-    r1_perimeter = shapes.perimeter_rectangle(4,5)
-    print(f"The perimeter of a rectangle with sides 4 and 5 is {r1_perimeter}")
-
-    t1_area = shapes.area_triangle(2.3, 4.5)
-    print(f"The area of a triangle with base 2.3 and height 4.5 is {t1_area}")
-
-    t1_perimeter = shapes.perimeter_triangle(2.3, 4.5, 6.7)
-    print(f"The perimeter of a triangle with sides 2.3, 4.5, and 6.7 is {t1_perimeter}")
+    # TODO: Implement the shapes module
 
 if __name__== "__main__":
   main()
--- shapes.py
+++ shapes.py
@@ -1,15 +1,11 @@
-def area_rectangle(l: float,w: float):
-    """Calculates the area of a rectangle with the formula l*w"""
+def area_rectangle(l,w):
     return l*w
 
-def perimeter_rectangle(l:float, w:float):
-    """Calculates the perimeter of a rectangle with the formula l*2 + w*2"""
+def perimeter_rectangle(l,w):
     return l*2+w*2
 
-def area_triangle(b:float, h:float):
-    """Calculates the area of a triangle with the formula b*h"""
+def area_triangle(b,h):
     return .5*b*h
 
-def perimeter_triangle(a:float, b:float, c:float):
-    """Calculates the perimeter of a triangle with the formula a+b+c"""
+def perimeter_triangle(a,b,c):
     return a+b+c
