8bd5dd8b188b7c2dee1e1c83b02db0095c3e4203
Rob Elliott <elliott@iupui.edu>
2023-08-25 12:46:52 -0400

    Final code

---
+++ README.md
@@ -0,0 +1,11 @@
+# Basics of Creating Python Modules
+The sample code above demonstrates how to create a local Python module and use it in another Python file.
+
+## Topics
+* Creating local Python module
+* Importing and using a local module
+* Documenting modules with docstrings
+
+## Organization
+* The `start` branch contains starter code for you to use while following the tutorials
+* The `final` branch contains the code at the conclusion of the tutorials
---
+++ shapes.py
@@ -0,0 +1,15 @@
+def area_rectangle(l: float,w: float):
+    """Calculates the area of a rectangle with the formula l*w"""
+    return l*w
+
+def perimeter_rectangle(l:float, w:float):
+    """Calculates the perimeter of a rectangle with the formula l*2 + w*2"""
+    return l*2+w*2
+
+def area_triangle(b:float, h:float):
+    """Calculates the area of a triangle with the formula b*h"""
+    return .5*b*h
+
+def perimeter_triangle(a:float, b:float, c:float):
+    """Calculates the perimeter of a triangle with the formula a+b+c"""
+    return a+b+c
